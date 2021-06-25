5547d40999a4b75612e3933da809b969b053caac
supakit25433 <supakrit.kit@mail.kmutt.ac.th>
2021-06-25 15:12:27 +0700

    Add WebConfig

---
+++ src/main/java/com/example/portal/api/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.portal.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	public void addCorsMapping(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowCredentials(false).maxAge(3600);
+	}
+	
+}
