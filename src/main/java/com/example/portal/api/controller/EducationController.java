7b26632a98a92b2a9abc0a2e46e95b6c8a58e946
supakit25433 <supakrit.kit@mail.kmutt.ac.th>
2021-06-25 14:54:28 +0700

    Add CORS in EducationController

--- src/main/java/com/example/portal/api/controller/EducationController.java
+++ src/main/java/com/example/portal/api/controller/EducationController.java
@@ -3,12 +3,14 @@ package com.example.portal.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.portal.api.repository.EducationRepository;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/v1")
 public class EducationController {
@@ -16,6 +18,7 @@ public class EducationController {
 	@Autowired
 	EducationRepository er;
 	
+	@CrossOrigin("http://localhost")
 	@RequestMapping(path = "/education/university", method = RequestMethod.GET)
 	public List<Object> getUniversityList() {
 		List<Object> ul = er.getUniversity();
