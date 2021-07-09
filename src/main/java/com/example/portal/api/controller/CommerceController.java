a92aeddea7a3f487766e828c14b3d2cdab96e6cd
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-07-09 16:01:47 +0700

    [EDIT] add other API and delete comment not use

--- src/main/java/com/example/portal/api/controller/CommerceController.java
+++ src/main/java/com/example/portal/api/controller/CommerceController.java
@@ -12,14 +12,78 @@ import com.example.portal.api.repository.CommerceRepository;
 @RestController
 @RequestMapping("/v1/commerce")
 public class CommerceController {
-	
+
 	@Autowired
 	CommerceRepository cr;
-	
-	@RequestMapping(path = "/permanentsecretary/importerexporterlist", method = RequestMethod.GET)
-	public List<Object> getCPFImporterExporter(){
+
+	// unable to find valid certification path to requested target
+	@RequestMapping(path = "/importerexporterlist", method = RequestMethod.GET)
+	public List<Object> getCPFImporterExporter() {
 		List<Object> ie = cr.getCPFimporterexporter();
 		return ie;
 	}
+
+	// unable to find valid certification path to requested target
+	@RequestMapping(path = "/department/member/type", method = RequestMethod.GET)
+	public List<Object> getDepartmentMemberType() {
+		List<Object> ie = cr.getDepartmentMemberType();
+		return ie;
+	}
+
+	// unable to find valid certification path to requested target
+	@RequestMapping(path = "/rice/trader", method = RequestMethod.GET)
+	public List<Object> getRiceTrader() {
+		List<Object> ie = cr.getRiceTrader();
+		return ie;
+	}
+
+	// unable to find valid certification path to requested target
+	@RequestMapping(path = "/department/member/search/bytype", method = RequestMethod.GET)
+	public List<Object> getSearchDepartmentMemberByType() {
+		List<Object> ie = cr.getSearchDepartmentMemberByType();
+		return ie;
+	}
+
+	// unable to find valid certification path to requested target
+	@RequestMapping(path = "/copyright/search/bykeyword", method = RequestMethod.GET)
+	public List<Object> getSearchCopyrightByKeyword() {
+		List<Object> ie = cr.getSearchCopyrightByKeyword();
+		return ie;
+	}
+
+	// unable to find valid certification path to requested target
+	@RequestMapping(path = "/trademark/search/bykeyword", method = RequestMethod.GET)
+	public List<Object> getSearchTrademarkByKeyword() {
+		List<Object> ie = cr.getSearchTrademarkByKeyword();
+		return ie;
+	}
+
+	// unable to find valid certification path to requested target
+	@RequestMapping(path = "/product/information", method = RequestMethod.GET)
+	public List<Object> getProductInformation() {
+		List<Object> ie = cr.getProductInformation();
+		return ie;
+	}
+
+	// unable to find valid certification path to requested target
+	@RequestMapping(path = "/thailand/internationaltrade/overview", method = RequestMethod.GET)
+	public List<Object> getThailandInternationalOverview() {
+		List<Object> ie = cr.getThailandInternationalOverview();
+		return ie;
+	}
+
+	// unable to find valid certification path to requested target
+	@RequestMapping(path = "/agricultural/products", method = RequestMethod.GET)
+	public List<Object> getAgriculturalProduct() {
+		List<Object> ie = cr.getAgriculturalProduct();
+		return ie;
+	}
+
+	// unable to find valid certification path to requested target
+		@RequestMapping(path = "/activity/type", method = RequestMethod.GET)
+		public List<Object> getActivityType() {
+			List<Object> ie = cr.getActivityType();
+			return ie;
+		}
 	
 }
--- src/main/java/com/example/portal/api/repository/CommerceRepository.java
+++ src/main/java/com/example/portal/api/repository/CommerceRepository.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class CommerceRepository {
 
-	// unable to find valid certification path to requested target
 	public List<Object> getCPFimporterexporter() {
 		StringBuilder responseStrBuilder = new StringBuilder();
 		try {
@@ -41,7 +40,267 @@ public class CommerceRepository {
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-//		System.out.println(responseStrBuilder.toString());
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getDepartmentMemberType() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://dataapi.moc.go.th/ditp-member-types");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.defaultCharset()));
+				String line = "";
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getRiceTrader() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://dataapi.moc.go.th/rice-entrepreneurs?name_keyword=สยาม");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.defaultCharset()));
+				String line = "";
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getSearchDepartmentMemberByType() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://dataapi.moc.go.th/ditp-members?member_type_id=2&limit=2");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.defaultCharset()));
+				String line = "";
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getSearchCopyrightByKeyword() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://dataapi.moc.go.th/copyright-details-by-keyword?keyword=มะขามป้อม");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.defaultCharset()));
+				String line = "";
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getSearchTrademarkByKeyword() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://dataapi.moc.go.th/trademark-details-by-keyword?keyword=มะขามป้อม");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.defaultCharset()));
+				String line = "";
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getProductInformation() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://dataapi.moc.go.th/products?keyword=มะพร้าว");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.defaultCharset()));
+				String line = "";
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getThailandInternationalOverview() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://dataapi.moc.go.th/summary-countries?year=2017&month=12");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.defaultCharset()));
+				String line = "";
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getAgriculturalProduct() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://dataapi.moc.go.th/gis-products?keyword=มะพร้าว&sell_type=wholesale");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.defaultCharset()));
+				String line = "";
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getActivityType() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://dataapi.moc.go.th/ditp-activity-types");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.defaultCharset()));
+				String line = "";
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 		JSONArray result = new JSONArray(responseStrBuilder.toString());
 		List<Object> al = result.toList();
 		return al;
