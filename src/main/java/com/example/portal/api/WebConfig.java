19c5487aa23347b3cc0a8d073c24c62616e6508a
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-07-09 10:14:46 +0700

    [FIX] Fix WebMvcConfigurerAdapter deprecated

--- src/main/java/com/example/portal/api/WebConfig.java
+++ src/main/java/com/example/portal/api/WebConfig.java
@@ -3,11 +3,11 @@ package com.example.portal.api;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
 	public void addCorsMapping(CorsRegistry registry) {
 		registry.addMapping("/**")
