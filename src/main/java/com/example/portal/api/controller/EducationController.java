1aa02ac4e6c420c4d9a2d11a95ed7ce38926f1c1
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-07-09 10:06:06 +0700

    [EDIT] change path in Education Controller & add code for other api

--- src/main/java/com/example/portal/api/controller/EducationController.java
+++ src/main/java/com/example/portal/api/controller/EducationController.java
@@ -3,37 +3,136 @@ package com.example.portal.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.portal.api.repository.EducationRepository;
 
-@CrossOrigin(maxAge = 3600)
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/education")
 public class EducationController {
 
 	@Autowired
 	EducationRepository er;
 	
-	@CrossOrigin("http://localhost")
-	@RequestMapping(path = "/education/university", method = RequestMethod.GET)
+	@RequestMapping(path = "/university/information/2563", method = RequestMethod.GET)
 	public List<Object> getUniversityList() {
 		List<Object> ul = er.getUniversity();
 		return ul;
 	}
 	
-	@RequestMapping(path = "/education/demonstration", method = RequestMethod.GET)
+	@RequestMapping(path = "/demonstration/list", method = RequestMethod.GET)
 	public List<Object> getDemonstrationList() {
 		List<Object> dl = er.getDemonstration();
 		return dl;
 	}
 	
-	@RequestMapping(path = "/education/demonstration/numandsex", method = RequestMethod.GET)
-	public List<Object> getDemonstrationNumAndSex() {
-		List<Object> dnsl = er.getDemonstrationNumAndSex();
+	@RequestMapping(path = "/demonstration/list/bynameandgender/2563", method = RequestMethod.GET)
+	public List<Object> getDemonstrationNameAndGender() {
+		List<Object> dnsl = er.getDemonstrationNameAndGender();
+		return dnsl;
+	}
+	
+	@RequestMapping(path = "/data/university/2563", method = RequestMethod.GET)
+	public List<Object> getUniversityData2563() {
+		List<Object> dnsl = er.getUniversityData2563();
+		return dnsl;
+	}
+	
+	@RequestMapping(path = "/data/workcondition/2563", method = RequestMethod.GET)
+	public List<Object> getWorkCondition2563() {
+		List<Object> dnsl = er.getWorkCondition2563();
+		return dnsl;
+	}
+	
+	@RequestMapping(path = "/data/graduate/2563", method = RequestMethod.GET)
+	public List<Object> getGraduateData2563() {
+		List<Object> dnsl = er.getGraduateData2563();
+		return dnsl;
+	}
+	
+	@RequestMapping(path = "/data/staff/2563", method = RequestMethod.GET)
+	public List<Object> getStaffData2563() {
+		List<Object> dnsl = er.getStaffData2563();
+		return dnsl;
+	}
+	
+	@RequestMapping(path = "/data/student/2563", method = RequestMethod.GET)
+	public List<Object> getStudentData2563() {
+		List<Object> dnsl = er.getStudentData2563();
+		return dnsl;
+	}
+	
+	@RequestMapping(path = "/university/course", method = RequestMethod.GET)
+	public List<Object> getUniversityCourse() {
+		List<Object> dnsl = er.getUniversityCourse();
+		return dnsl;
+	}
+	
+	// long load
+	@RequestMapping(path = "/university/graduate/number/byinstitution/2562", method = RequestMethod.GET)
+	public List<Object> getNumberOfGraduateByInstitution2562() {
+		List<Object> dnsl = er.getNumberOfGraduateByInstitution2562();
+		return dnsl;
+	}
+	
+	@RequestMapping(path = "/university/list/bygroup", method = RequestMethod.GET)
+	public List<Object> getUniversityListByGroup() {
+		List<Object> dnsl = er.getUniversityListByGroup();
+		return dnsl;
+	}
+	
+	@RequestMapping(path = "/university/graduate/number/byfacultyanddepartment/2562", method = RequestMethod.GET)
+	public List<Object> getNumberOfGraduateByFacultyAndDepartment2562() {
+		List<Object> dnsl = er.getNumberOfGraduateByFacultyAndDepartment2562();
+		return dnsl;
+	}
+	
+	@RequestMapping(path = "/university/student/number/2562", method = RequestMethod.GET)
+	public List<Object> getNumberOfStudent2562() {
+		List<Object> dnsl = er.getNumberOfStudent2562();
+		return dnsl;
+	}
+	
+	@RequestMapping(path = "/university/staff/number/2563", method = RequestMethod.GET)
+	public List<Object> getNumberOfStaff2563() {
+		List<Object> dnsl = er.getNumberOfStaff2563();
+		return dnsl;
+	}
+	
+	// server error - not found
+	@RequestMapping(path = "/researcher/list/economicanimal", method = RequestMethod.GET)
+	public List<Object> getResearcherListOfEconomicAnimal() {
+		List<Object> dnsl = er.getResearcherListOfEconomicAnimal();
+		return dnsl;
+	}
+	
+	// server error - not found
+	@RequestMapping(path = "/researcher/list/energy", method = RequestMethod.GET)
+	public List<Object> getResearcherListOfEnergy() {
+		List<Object> dnsl = er.getResearcherListOfEnergy();
+		return dnsl;
+	}
+	
+	// server error - not found
+	@RequestMapping(path = "/researcher/list/technology", method = RequestMethod.GET)
+	public List<Object> getResearcherListOfTechnology() {
+		List<Object> dnsl = er.getResearcherListOfTechnology();
+		return dnsl;
+	}
+	
+	// server error - not found
+	@RequestMapping(path = "/researcher/list/watermanagement", method = RequestMethod.GET)
+	public List<Object> getResearcherListOfWaterManagement() {
+		List<Object> dnsl = er.getResearcherListOfWaterManagement();
+		return dnsl;
+	}
+	
+	// server error - not found
+	@RequestMapping(path = "/researcher/list/zerowaste", method = RequestMethod.GET)
+	public List<Object> getResearcherListOfZeroWaste() {
+		List<Object> dnsl = er.getResearcherListOfZeroWaste();
 		return dnsl;
 	}
 	
--- src/main/java/com/example/portal/api/repository/EducationRepository.java
+++ src/main/java/com/example/portal/api/repository/EducationRepository.java
@@ -74,10 +74,481 @@ public class EducationRepository {
 		return al;
 	}
 	
-	public List<Object> getDemonstrationNumAndSex() {
+	public List<Object> getDemonstrationNameAndGender() {
 		StringBuilder responseStrBuilder = new StringBuilder();
 		try {
 			URL url = new URL("http://202.44.139.145/api/public/opendata/SATIT_STD_11_03");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getUniversityData2563() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("http://202.44.139.145/api/public/opendata/5_2563UOC_UNIV");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getWorkCondition2563() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("http://202.44.139.145/api/public/opendata/4_2563UOC_QUESTIONNAIRE");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getGraduateData2563() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("http://202.44.139.145/api/public/opendata/3_2563uoc_graduate");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getStaffData2563() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("http://202.44.139.145/api/public/opendata/2_2563uoc_staff");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getStudentData2563() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("http://202.44.139.145/api/public/opendata/1_2563uoc_student");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getUniversityCourse() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("http://202.44.139.145/api/public/opendata/UNIV_CUR_11_01");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	// Slow Response
+	public List<Object> getNumberOfGraduateByInstitution2562() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("http://202.44.139.145/api/public/opendata/UNIV_GRD_11_02");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getUniversityListByGroup() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("http://202.44.139.145/api/public/opendata/UNIV_UNI_11_01");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getNumberOfGraduateByFacultyAndDepartment2562() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("http://202.44.139.145/api/public/opendata/UNIV_GRD_11_01");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getNumberOfStudent2562() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("http://202.44.139.145/api/public/opendata/UNIV_STD_11_02");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	public List<Object> getNumberOfStaff2563() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("http://202.44.139.145/api/public/opendata/UNIV_STF_12_01");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	// Response --> Text
+	public List<Object> getResearcherListOfEconomicAnimal() {
+//		StringBuilder responseStrBuilder = new StringBuilder();
+		String responseStrBuilder = "";
+		try {
+			URL url = new URL("https://api.nriis.go.th/service/service/OpenData/v1/DataSet/Researcher_EcoAnimal");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder = responseStrBuilder + line;	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder);
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	// Response --> Text
+	public List<Object> getResearcherListOfEnergy() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://api.nriis.go.th/service/service/OpenData/v1/DataSet/Researcher_Energy");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	// Response --> Text
+	public List<Object> getResearcherListOfTechnology() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://api.nriis.go.th/service/service/OpenData/v1/DataSet/Researcher_TectIndustry");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	// Response --> Text
+	public List<Object> getResearcherListOfWaterManagement() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://api.nriis.go.th/service/service/OpenData/v1/DataSet/Researcher_WaterManagement");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			int responseCode = connection.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {		
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("TIS-620")));
+				String line = "";	
+				while((line = br.readLine()) != null) {
+					responseStrBuilder.append(line);	
+				}
+				connection.getInputStream().close();
+			}
+			connection.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		JSONArray result = new JSONArray(responseStrBuilder.toString());
+		List<Object> al = result.toList();
+		return al;
+	}
+	
+	// Response --> Text
+	public List<Object> getResearcherListOfZeroWaste() {
+		StringBuilder responseStrBuilder = new StringBuilder();
+		try {
+			URL url = new URL("https://api.nriis.go.th/service/service/OpenData/v1/DataSet/Researcher_Zerowaste");
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
 			connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
