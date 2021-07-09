0bc776ed4dd0547d38d27ea46d52e4194841a286
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-07-09 16:00:27 +0700

    [FIX] fix warning

--- src/main/java/com/example/portal/api/WebConfig.java
+++ src/main/java/com/example/portal/api/WebConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
 	public void addCorsMapping(CorsRegistry registry) {
-		registry.addMapping("/**")
-		.allowCredentials(false).maxAge(3600);
+		registry.addMapping("/**");
 	}
 	
 }
