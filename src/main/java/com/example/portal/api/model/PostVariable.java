be4c2ead932c79160c5ae5c30bd0faa42631f71e
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-07-15 19:33:26 +0700

    [ADD] add PostVariable class for receive in post method

---
+++ src/main/java/com/example/portal/api/model/PostVariable.java
@@ -0,0 +1,15 @@
+package com.example.portal.api.model;
+
+public class PostVariable {
+
+	private String path_variable;
+
+	public String getPath_variable() {
+		return path_variable;
+	}
+
+	public void setPath_variable(String path_variable) {
+		this.path_variable = path_variable;
+	}
+	
+}
